da865b1a3c713cba933d94e767e608a7be4219b5
Erik Bayerlein <erik.bayer@icloud.com>
2024-03-23 11:20:25 -0300

    changing set_pixel and wave

--- draw/set_pixel.py
+++ draw/set_pixel.py
@@ -2,9 +2,6 @@ import pygame
 
 
 def set_pixel(screen, x, y, color):
-    # estabelecendo um plano cartesiano regular
-    y = screen.get_height() - y
-
     if x < 0:
         x = 0
     if y < 0:
